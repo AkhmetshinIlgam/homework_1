5d715aad4c176786ad1f71aad8b08e4bf9ab6843
https://github.com/AkhmetshinIlgam/homework_1.git <akhmetshinilgam2013@gmail.com>
2025-01-10 15:45:13 +0500

    start

---
+++ Ucheba_28.12.24.py
@@ -0,0 +1,64 @@
+# x=(14+30)
+# y=((x+2)/2)
+# print(y)
+# print(x)
+# print()
+# print("это мои вычисления")
+
+
+# r=(2500/50)
+# x=(1000/200)
+# c=(81/9)
+# b=(20*5*2)
+# n=(r+x+c+b)
+# print(r)
+# print(x)
+# print(c)
+# print(b)
+# print(n)
+# print()
+
+
+# 10.01.24
+# 1. Написать код для суммы трех чисел, там же — разности этих же чисел.
+# the sum of the numbers:
+# option-1
+Q=5
+S=10
+Z=2
+B=(Q+S+Z)
+resoult = Q+S+Z
+print(B)
+print(resoult)
+print(5+10+2)
+print()
+# option-2
+A=(5+2+10)
+print(A)
+print()
+# the difference of numbers:
+# option-1
+Q=5
+S=10
+Z=2
+B=(S-Q-Z)
+print(B)
+print()
+# option-2
+A=(10-5-2)
+print(A)
+print()
+# 2. Написать код для произведения трех чисел.
+# option-1
+Q=5
+S=10
+Z=2
+B=(Q*S*Z)
+print(B)
+print()
+# option-2
+A=(5*10*2)
+print(A)
+print()
+# 3. Вывести на экран четверостишие (отрывок из песни/стихотворения) пока в виде строки.
+print("Буря мглою небо кроет, Вихри снежные крутя; То, как зверь, она завоет, То заплачет, как дитя.")
